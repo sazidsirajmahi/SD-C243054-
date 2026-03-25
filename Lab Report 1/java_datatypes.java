b825c417dc6665711111a086062100948d2d510d
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-25 21:02:21 +0600

    Create java_datatypes.java

---
+++ Lab Report 1/java_datatypes.java
@@ -0,0 +1,10 @@
+public class java_datatypes{
+    public static void main(String[] args){
+        int a=20;
+        String s="mahi";
+        char c='m';
+        double pi=3.1426;
+        System.out.println(a +" "+s+" "+c+" "+pi);
+      
+    }
+}
