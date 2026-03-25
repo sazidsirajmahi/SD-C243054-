32be28468fbb73bd5fe8d1a6aaa37cb60e60c739
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-25 21:21:50 +0600

    Create java_operators.java

---
+++ Lab Report 1/java_operators.java
@@ -0,0 +1,11 @@
+public class java_operators{
+    public static void main(String[] args){
+       int a,b,sum,multip;
+       a=50;
+       b=3;
+       sum=a+b;
+       multip=a*b;
+        System.out.println(sum +" "+" "+multip);
+      
+    }
+}
