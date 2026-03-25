7c4d952c9c9b0889868fcd8e9039e4ccdb9d1f34
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-25 20:14:39 +0600

    Create java_user_input.java

---
+++ Lab Report 1/java_user_input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class java_user_input {
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        String age=s.nextLine();
+        System.out.println("Your age is " + age);
+        
+    }
+}
