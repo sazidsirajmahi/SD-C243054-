cfe26a36b04d89bbabc5320d12f52c5a9db8b088
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-25 20:45:56 +0600

    Create java_comments.java

---
+++ Lab Report 1/java_comments.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class java_comments {
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        String age=s.nextLine();
+        /*
+        first i created an object to scan the input 
+        then using it stored the input in a string
+         */
+        System.out.println("Your age is " + age); //this is a single comment
+        
+    }
+}
