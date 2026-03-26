3d5a154bede27bd7fb4b04b4ee2d86d1018716fc
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-26 09:46:12 +0600

    Create java_boolean.java

---
+++ Lab Report 1/java_boolean.java
@@ -0,0 +1,11 @@
+public class java_boolean{
+    public static void main(String[] args){
+       int a,b;
+       a=10;
+       b=3;
+       
+        System.out.println(a>b);
+        System.out.println(a==10);
+          
+    }
+}
