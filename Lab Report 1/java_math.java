a7d4668893b381773a8ad96b41398975516e1c5c
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-26 09:41:15 +0600

    Create java_math.java

---
+++ Lab Report 1/java_math.java
@@ -0,0 +1,12 @@
+public class java_math{
+    public static void main(String[] args){
+       int a,b;
+       a=10;
+       b=3;
+       
+        System.out.println(Math.min(a,b));
+          System.out.println(Math.max(a,b));
+              System.out.println(Math.sqrt(25));
+
+    }
+}
