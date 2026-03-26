c9511c01eae07a72b5980c68fbdf3575e4165002
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-26 09:33:55 +0600

    Create java_strings.java

---
+++ Lab Report 1/java_strings.java
@@ -0,0 +1,9 @@
+public class java_strings{
+    public static void main(String[] args){
+        String s;
+        s="sazid SIRAJ MAhi";
+        System.out.println(s.toLowerCase());
+           System.out.println(s.toUpperCase());
+           System.out.println("Length of the strings =" + s.length());
+    }
+}
