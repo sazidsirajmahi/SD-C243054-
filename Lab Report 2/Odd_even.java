d5ce3d9fe03c7a54c1dc9eb63fcafaef2e63379f
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-29 19:40:51 +0600

    Create Odd_even.java

---
+++ Lab Report 2/Odd_even.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Odd_even{
+    public static void main(String[] arg){
+       Scanner s= new Scanner(System.in);
+       int name=s.nextInt();
+       if(name%2==0){
+        System.out.println("The number is even");
+       }
+       else
+          System.out.println("The number is odd");
+       }
+    }
