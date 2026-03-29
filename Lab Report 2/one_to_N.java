be6afa7a7ec12a4aa1f3e6604cc06795227effae
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-29 20:11:41 +0600

    Create one_to_N.java

---
+++ Lab Report 2/one_to_N.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class one_to_N{
+    public static void main(String[] arg){
+       Scanner sc = new Scanner(System.in);    
+        int a = sc.nextInt();
+      
+       int i=1;
+       while(i<=a){
+        System.out.print(i+" ");
+        i++;
+       }
+
+    }
+}
