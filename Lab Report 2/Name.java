1f45239a513c1842fdd35fe75617518824d9c4a4
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-03-29 19:43:02 +0600

    Create Name.java

---
+++ Lab Report 2/Name.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Name{
+    public static void main(String[] arg){
+       Scanner s= new Scanner(System.in);
+       String name=s.nextLine();
+       System.out.println("Your name is "+name);
+    }
+}
