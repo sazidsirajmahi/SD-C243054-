cdfabb28b25ea199268c763fc2fd7d229147576c
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-05-01 17:05:58 +0600

    Create 10No.java

---
+++ Lab Report 2/set3/10No.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class bottles{
+public static void main(String[]args){
+ Scanner sc=new Scanner(System.in);
+  int b1=sc.nextInt();
+ int b2=sc.nextInt();
+ int b3=sc.nextInt();
+ int empty=0;
+if(b1==0)
+  empty++;
+if(b2==0)
+  empty++;
+if(b3==0)
+  empty++;
+if(empty>=2)
+  System.out.println("Water filling time");
+else 
+  System.out.println("Not now");
+sc.close();
+}
+}
