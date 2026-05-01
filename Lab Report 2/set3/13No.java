f484686e253cab2d56040326561c7b1fb2217edb
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-05-01 20:03:06 +0600

    Create 13No.java

---
+++ Lab Report 2/set3/13No.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ public class 13No{
+ public static void main(String[]args){
+  Scanner sc=new Scanner(System.in);
+ int a=sc.nextInt();
+ int b=sc.nextInt();
+  int c=sc.nextInt();
+ int x=sc.nextInt();
+if(x==a||x==b||x==c)
+  System.out.println("YES");
+else 
+  System.out.println("NO");
+  sc.close();
+      }
+ }
