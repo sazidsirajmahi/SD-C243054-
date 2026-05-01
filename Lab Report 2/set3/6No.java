457e1a10d263009c155d8715c6b3b59c77a01be0
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-05-01 16:49:19 +0600

    Create 6No.java

---
+++ Lab Report 2/set3/6No.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class 6No{
+   public static void main(String[]args){
+ Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int x=sc.nextInt();
+   int sub=(n+5)/6;
+  int total=sub*x;
+   System.out.println(total);
+    sc.close();
+       }
+  }
