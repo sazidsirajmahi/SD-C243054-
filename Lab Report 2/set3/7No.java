bfb02b22ec4dd0873d74f64212dd3d4a9a8f2918
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-05-01 16:52:34 +0600

    Create 7No.java

---
+++ Lab Report 2/set3/7No.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class assignment{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+if(x+3<=10)System.out.println("YES");
+else System.out.println("NO");
+sc.close();
+}
+}
