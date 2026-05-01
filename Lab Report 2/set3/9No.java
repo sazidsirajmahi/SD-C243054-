9b5b01ede972c99e8eae8d047899955f4b8b6452
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-05-01 17:00:31 +0600

    Create 9No.java

---
+++ Lab Report 2/set3/9No.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class workinghours{
+ public static void main(String[]args){
+   Scanner sc=new Scanner(System.in);
+ int x=sc.nextInt();
+ int y=sc.nextInt();
+ int total=(4*x)+y;
+   System.out.println(total);
+sc.close();
+}
+}
