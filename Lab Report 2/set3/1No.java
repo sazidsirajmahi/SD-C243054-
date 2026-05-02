c41578f3fa49c150cb90d65fd1cca98f4b794b42
sazidsirajmahi <sazidsirajmahi7@gmail.com>
2026-05-02 19:08:06 +0600

    Update 1No.java

--- Lab Report 2/set3/1No.java
+++ Lab Report 2/set3/1No.java
@@ -1,18 +1,12 @@
 import java.util.Scanner;
-
-public class 1No {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        int num1 = input.nextInt();
-        int num2 = input.nextInt();
-
-        if ((num1 + num2) % 2 == 0) {
-            System.out.println("YES. The average of " + num1 + " and " + num2 + " is an Integer. " + (num1 + num2) / 2);
-        } else {
-            System.out.println("No. The average of " + num1 + " and " + num2 + " is not an Integer.");
-        }
-
-        input.close();
+public class 1No{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int c=sc.nextInt();
+        if((a+c)%2==0)
+            System.out.println("yes"+" "+((a+c)/2));
+        else
+            System.out.println("no");
     }
 }
